ac27f813abf7d549fce954b6990087b38a8a6005
elisaalcala <elisaalcgue@gmail.com>
2024-11-17 23:16:10 +0100

    fix: ForwardedHeaderFilter

--- src/main/java/com/app/alcala/configuration/SecurityConfiguration.java
+++ src/main/java/com/app/alcala/configuration/SecurityConfiguration.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import com.app.alcala.service.impl.RepositoryUserDetailsService;
 
@@ -62,5 +62,10 @@ public class SecurityConfiguration {
 
 		return http.build();
 	}
+    
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter(); 
+    }
 
 }
